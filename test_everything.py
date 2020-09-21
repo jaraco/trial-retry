55a2103a3d346bca121f0d2f37b932e478077b40
Jason R. Coombs <jaraco@jaraco.com>
2020-09-21 11:13:59 -0400

    Add Deferred tests, though they never fail.

--- test_everything.py
+++ test_everything.py
@@ -1,19 +1,39 @@
 import unittest
 import random
 
+from twisted.internet.defer import Deferred
 
-flaky_rate = .2
+
+flaky_rate = 1
+
+
+def flaky_exception():
+    if random.random() < flaky_rate:
+        raise ValueError("flaky exception")
+
+def flaky_fail(test):
+    random.random() < flaky_rate and test.fail("flaky fail")
 
 
 class ThingsTest(unittest.TestCase):
     def test_simple_exception(self):
-        if random.random() < flaky_rate:
-            raise ValueError("flaky exception")
+        flaky_exception()
 
     def test_simple_failure(self):
-        if random.random() < flaky_rate:
-            self.fail("flaky fail")
+        flaky_fail(self)
 
     def test_simple_assertion(self):
         if random.random() < flaky_rate:
             assert False, "flaky assertion"
+
+
+class DeferredsTests(unittest.TestCase):
+    def test_simple_exception(self):
+        result = Deferred()
+        result.addCallback(flaky_exception)
+        return result
+
+    def test_simple_failure(self):
+        result = Deferred()
+        result.addCallback(flaky_fail, self)
+        return result
